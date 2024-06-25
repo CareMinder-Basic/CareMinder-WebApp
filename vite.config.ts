cd2705583170d606209c766013d689292239b949
nimod7890 <nimod1234@g.skku.edu>
2024-06-25 14:52:02 +0900

    feat: add svg library

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import macrosPlugin from "vite-plugin-babel-macros";
+import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [macrosPlugin(), react(), tsconfigPaths()],
+  plugins: [macrosPlugin(), react(), tsconfigPaths(), svgr()],
 });
