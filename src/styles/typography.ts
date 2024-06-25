90bed9c96d46df851d23a5c3d105f26f3d1260ef
nimod7890 <nimod1234@g.skku.edu>
2024-06-25 16:34:43 +0900

    fix: letter spacing units

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -10,32 +10,32 @@ const typography = {
     fontSize: calcRem(24),
     fontWeight: 500,
     lineHeight: 1.5,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   } /**   * @description Pretendard Medium, Font Size: 18px, Font Weight: 500   */,
   h2: {
     fontFamily: "Pretendard Medium",
     fontSize: calcRem(18),
     fontWeight: 500,
     lineHeight: 1.44,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   } /**   * @description Pretendard Medium, Font Size: 16px, Font Weight: normal   */,
   h3: {
     fontFamily: "Pretendard Medium",
     fontSize: calcRem(16),
     lineHeight: 1.5,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   } /**   * @description Pretendard Regular, Font Size: 14px, Font Weight: normal   */,
   body1: {
     fontFamily: "Pretendard Regular",
     fontSize: calcRem(14),
     lineHeight: 1.42,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   } /**   * @description Pretendard Medium, Font Size: 13px, Font Weight: normal   */,
   body2: {
     fontFamily: "Pretendard Medium",
     fontSize: calcRem(13),
     lineHeight: 1.54,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   },
   /**
    * @description Pretendard SemiBold, Font Size: 16px, Font Weight: normal
@@ -44,7 +44,7 @@ const typography = {
     fontFamily: "Pretendard SemiBold",
     fontSize: calcRem(16),
     lineHeight: 1.5,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   },
   /**
    * @description Pretendard SemiBold, Font Size: 14px, Font Weight: normal
@@ -53,7 +53,7 @@ const typography = {
     fontFamily: "Pretendard SemiBold",
     fontSize: calcRem(14),
     lineHeight: 1.42,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   },
   /**
    * @description Pretendard, Font Size: 12px, Font Weight: 600
@@ -62,7 +62,7 @@ const typography = {
     fontSize: calcRem(12),
     fontWeight: 600,
     lineHeight: 1.3,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   },
   /**
    * @description Pretendard, Font Size: 14px, Font Weight: 600
@@ -71,7 +71,7 @@ const typography = {
     fontSize: calcRem(14),
     fontWeight: 600,
     lineHeight: 1.2,
-    letterSpacing: "-3%",
+    letterSpacing: "-0.03em",
   },
 } as const;
 
