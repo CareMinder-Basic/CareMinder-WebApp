a21a0cf6f4c1844d364752f2df3e0fbc5529e409
nimod7890 <nimod1234@g.skku.edu>
2024-06-27 01:24:31 +0900

    docs: add todo(식단 페이지)

--- src/components/layout/sidebar/UserSidebar.tsx
+++ src/components/layout/sidebar/UserSidebar.tsx
@@ -11,6 +11,7 @@ export default function UserSidebar() {
   return (
     <>
       <HomeMenu />
+      {/* Todo */}
       {/* <DietMenu /> */}
       <RequestsMenu />
       <PatientManagementMenu />
