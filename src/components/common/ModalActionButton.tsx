0f3eea799da74f88aee2102ad2f2156858f0e54a
nimod7890 <nimod1234@g.skku.edu>
2024-06-27 20:43:32 +0900

    design: modal action button disabled, hover 시 스타일 조정

--- src/components/common/ModalActionButton.tsx
+++ src/components/common/ModalActionButton.tsx
@@ -4,27 +4,36 @@ type ModalActionButtonProps = {
   color?: "primary" | "secondary";
 } & Omit<ButtonProps, "color">;
 
-const ModalActionButton = styled(
-  (props: ButtonProps) => <Button variant="contained" {...props} />,
-  {
-    shouldForwardProp: prop => prop !== "variant",
-  },
-)<ModalActionButtonProps>(({ color = "primary", theme }) => ({
+const ModalActionButton = styled((props: ButtonProps) => <Button variant="outlined" {...props} />, {
+  shouldForwardProp: prop => prop !== "variant",
+})<ModalActionButtonProps>(({ color = "primary", theme }) => ({
   borderRadius: "100px",
   minWidth: "108px",
   padding: "6px 16px",
   height: "36px",
   fontSize: "16px",
+  fontWeight: 800,
   lineHeight: "24px",
   ...(color === "primary" && {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
+    "backgroundColor": theme.palette.primary.main,
+    "color": theme.palette.primary.contrastText,
+    "border": "none",
+    "&:hover": {
+      backgroundColor: theme.palette.primary.light,
+      border: "none",
+    },
+    "&:disabled": {
+      backgroundColor: theme.palette.primary.light,
+      border: "none",
+      color: theme.palette.primary.contrastText,
+    },
   }),
   ...(color === "secondary" && {
     "color": theme.palette.text.primary,
-    "backgroundColor": theme.palette.primary.contrastText,
     "borderColor": theme.palette.divider,
+    "backgroundColor": theme.palette.primary.contrastText,
     "&:hover": {
+      borderColor: theme.palette.divider,
       backgroundColor: theme.palette.primary.light,
     },
   }),
