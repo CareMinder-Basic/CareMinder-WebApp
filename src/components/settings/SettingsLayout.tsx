831c7867c686600462758bc932201423593e5bc5
dvp-tae <th3016@naver.com>
2024-09-03 17:22:21 +0900

    Design: 병동 설정 로그인 모달창 구현

---
+++ src/components/settings/SettingsLayout.tsx
@@ -0,0 +1,68 @@
+import SigninForm from "@components/signin/SigninForm";
+import { useSignin } from "@hooks/mutation";
+import { SigninFormData } from "@models/signin";
+import { Grid, Divider, styled, Stack, Link } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { Close } from "@mui/icons-material";
+import { SettingsHeader } from ".";
+
+type SettingsLayoutProps = {
+  onClose: (event?: object, reason?: "backdropClick" | "escapeKeyDown") => void;
+};
+
+export default function SettingsLayout({ onClose }: SettingsLayoutProps) {
+  const form = useForm<SigninFormData>();
+  const { mutate: signin } = useSignin();
+
+  return (
+    <Container item xs>
+      <Content>
+        <CloseButton onClick={onClose} />
+        <SettingsHeader />
+        <SigninForm form={form} onSubmit={signin} />
+      </Content>
+      <Footer divider={<Divider orientation="vertical" />}>
+        <Link href="#" variant="h3">
+          ID / PW 찾기
+        </Link>
+      </Footer>
+    </Container>
+  );
+}
+
+const Container = styled(Grid)({
+  position: "relative",
+  minWidth: "fit-content",
+
+  padding: "24px",
+  paddingBottom: "50px",
+
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+});
+
+const CloseButton = styled(Close)({
+  position: "absolute",
+  top: "0",
+  right: "0",
+  cursor: "pointer",
+});
+
+const Content = styled(Stack)({
+  margin: "30px 0 60px 0",
+  flex: 1,
+
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const Footer = styled(Stack)({
+  height: "24px",
+  gap: "14px",
+
+  flexDirection: "row",
+
+  justifyContent: "center",
+  alignItems: "center",
+});
--- src/pages/user/SettingsPage.tsx
+++ src/pages/user/SettingsPage.tsx
@@ -1,3 +1,16 @@
+import SettingsModal from "@components/settings/SettingsModal";
+import { useState } from "react";
+
 export default function SettingsPage() {
-  return <>SettingsPage</>;
+  // 로그인 되어있지 않은 상태에서 병동 설정 선택 시 로그인 모달 open
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <SettingsModal open={isModalOpen} onClose={handleCloseModal} />
+    </>
+  );
 }
