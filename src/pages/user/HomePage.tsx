c8197eb9bd8494dc52f06416dd013839d3a71b3d
DevTeddy123 <ab315601@naver.com>
2024-09-12 15:24:04 +0900

    Design : 페이지네이션 스타일

--- src/pages/user/HomePage.tsx
+++ src/pages/user/HomePage.tsx
@@ -1,10 +1,3 @@
-import PaginationComponent from "@components/common/pagination";
-
 export default function HomePage() {
-  // return <>HomePage</>;
-  return (
-    <>
-      <PaginationComponent totalPage={10} />
-    </>
-  );
+  return <>HomePage</>;
 }
