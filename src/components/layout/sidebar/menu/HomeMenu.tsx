b1981f4b56d01887226b2e866c706958cb79894d
DevTeddy123 <ab315601@naver.com>
2024-09-16 17:31:16 +0900

    Feat : user 정보에 따른 sideBar 조정

--- src/components/layout/sidebar/menu/HomeMenu.tsx
+++ src/components/layout/sidebar/menu/HomeMenu.tsx
@@ -1,8 +1,22 @@
 import { ReactComponent as Home } from "@assets/menuIcons/home.svg";
 import MenuLayout from "./MenuLayout";
 import RoutePath from "@routes/routePath";
-import { useLocation } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userState } from "@libraries/recoil";
+import { SwitchCase } from "@toss/react";
 
 export default function HomeMenu() {
-  return <MenuLayout routePath={RoutePath.Home} pageName="홈" icon={Home} />;
+  const user = useRecoilValue(userState);
+
+  return (
+    <SwitchCase
+      value={user!.type}
+      caseBy={{
+        main: <MenuLayout routePath={RoutePath.Home} pageName="병동 메인" icon={Home} />,
+        staff: (
+          <MenuLayout routePath={RoutePath.StaffHomePage} pageName="스태프 메인" icon={Home} />
+        ),
+      }}
+    />
+  );
 }
