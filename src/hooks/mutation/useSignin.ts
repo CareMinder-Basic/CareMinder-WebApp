6c5cd6daf1159d6d4386e3c50227823256f2f503
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-10-09 14:45:54 +0900

    Feat: signUp 로직 추가

--- src/hooks/mutation/useSignin.ts
+++ src/hooks/mutation/useSignin.ts
@@ -1,17 +1,33 @@
 import { SigninFormData } from "@models/signin";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
-import { SEVER_URL } from "@/constants/baseUrl";
+import axiosInstance from "@/utils/axiosInstance";
+import Cookies from "js-cookie";
+import { useSetRecoilState } from "recoil";
+import { userState } from "@libraries/recoil";
 
-const signin = async (data: SigninFormData) => {
-  return (await axios.post(`${SEVER_URL}/staff/login`, data)).data;
+const signin = async (useInfo: SigninFormData) => {
+  const res = await axiosInstance.post("/staff/login", useInfo);
+  if (res.data.accessToekn && res.data.refreshToken) {
+    Cookies.set("accessToekn", res.data.accessToekn);
+    Cookies.set("refreshToekn", res.data.refreshToekn);
+  }
+  return res.data;
 };
 
 export default function useSignin() {
+  const setUserState = useSetRecoilState(userState);
   return useMutation({
     mutationFn: signin,
-    onSuccess: () => {
-      // Todo: page routing
+    onSuccess: res => {
+      console.log("로그인 성공");
+      setUserState({
+        id: res.id,
+        name: res.name,
+        type: res.type,
+      });
+    },
+    onError: error => {
+      console.error("로그인 실패:", error);
     },
   });
 }
