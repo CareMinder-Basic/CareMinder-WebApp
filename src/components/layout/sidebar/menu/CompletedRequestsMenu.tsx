0216bb101758bcad517e06efb41e4a13eb60e2e1
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-10-15 23:28:16 +0900

    FixL modalState:flase

--- src/components/layout/sidebar/menu/CompletedRequestsMenu.tsx
+++ src/components/layout/sidebar/menu/CompletedRequestsMenu.tsx
@@ -1,13 +1,32 @@
 import { ReactComponent as CompletedRequests } from "@assets/menuIcons/completedRequests.svg";
 import MenuLayout from "./MenuLayout";
 import RoutePath from "@routes/routePath";
+import { UserType } from "@models/user";
+import { SwitchCase } from "@toss/react";
+import { useRecoilValue } from "recoil";
+import { userState } from "@libraries/recoil";
 
 export default function CompletedRequestsMenu() {
+  const user = useRecoilValue(userState);
   return (
-    <MenuLayout
-      routePath={RoutePath.StaffCompletedRequests}
-      pageName={`완료 요청\n히스토리`}
-      icon={CompletedRequests}
+    <SwitchCase
+      value={user?.type as UserType}
+      caseBy={{
+        main: (
+          <MenuLayout
+            routePath={RoutePath.StaffCompletedRequests}
+            pageName={`완료 요청\n히스토리`}
+            icon={CompletedRequests}
+          />
+        ),
+        staff: (
+          <MenuLayout
+            routePath={RoutePath.StaffCompletedRequests}
+            pageName={`완료 요청\n히스토리`}
+            icon={CompletedRequests}
+          />
+        ),
+      }}
     />
   );
 }
--- src/components/layout/sidebar/menu/NoticeMenu.tsx
+++ src/components/layout/sidebar/menu/NoticeMenu.tsx
@@ -1,16 +1,20 @@
 import { ReactComponent as Notice } from "@assets/menuIcons/notice.svg";
 import MenuLayout from "./MenuLayout";
 import RoutePath from "@routes/routePath";
+import { useRecoilValue } from "recoil";
+import { userState } from "@libraries/recoil";
+import { UserType } from "@models/user";
+import { SwitchCase } from "@toss/react";
 
 export default function NoticeMenu() {
+  const user = useRecoilValue(userState);
   return (
-    // <SwitchCase
-    //   value={user?.type as UserType}
-    //   caseBy={{
-    //     main: <MenuLayout routePath={RoutePath.StaffNotice} pageName={"공지"} icon={Notice} />,
-    //     staff: <MenuLayout routePath={RoutePath.StaffNotice} pageName={"공지"} icon={Notice} />,
-    //   }}
-    // />
-    <MenuLayout routePath={RoutePath.StaffNotice} pageName={"공지"} icon={Notice} />
+    <SwitchCase
+      value={user?.type as UserType}
+      caseBy={{
+        main: <MenuLayout routePath={RoutePath.StaffNotice} pageName={"공지"} icon={Notice} />,
+        staff: <MenuLayout routePath={RoutePath.StaffNotice} pageName={"공지"} icon={Notice} />,
+      }}
+    />
   );
 }
