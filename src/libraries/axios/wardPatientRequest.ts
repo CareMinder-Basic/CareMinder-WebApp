ba5e464f65d8e819f711bb3f07a9f79ca4ec7eec
DevTeddy123 <ab315601@naver.com>
2024-10-19 14:42:30 +0900

    Feat : 대기 환자 조회 추가

--- src/libraries/axios/wardPatientRequest.ts
+++ src/libraries/axios/wardPatientRequest.ts
@@ -11,7 +11,7 @@ const PATH = "/ward-patient-requests";
 const WardPatientRequest: WardPatientRequestApiType = {
   //현재 병동에서 대기중인 환자 요청 조회
   getPending() {
-    return axiosInstance.get(PATH + "/pending");
+    return axiosInstance.get(PATH + "/pending", { params: { aiRole: null, myArea: false } });
   },
 
   //현재 병동의 직원들이 수락 중인 환자의 요청 조회
