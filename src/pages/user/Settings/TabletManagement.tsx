e7693d53f743ea91d19a8d8b3b339e40e4a14ca6
dvp-tae <th3016@naver.com>
2024-10-24 16:03:54 +0900

    feat: 태블릿 병상 관리 검색창 추가

--- src/pages/user/Settings/TabletManagement.tsx
+++ src/pages/user/Settings/TabletManagement.tsx
@@ -1,8 +1,10 @@
 import { Box, styled } from "@mui/system";
 import PaginationComponent from "@components/common/pagination";
-import { Typography } from "@mui/material";
+import { IconButton, InputBase, Paper, Typography } from "@mui/material";
 import TabletManagementTable from "@components/settings/TabletManagementTable";
 import CButton from "@components/common/atom/C-Button";
+
+import { ReactComponent as Search } from "@/assets/serachIcons/search-gray.svg";
 
 //태블릿 병상 관리
 
@@ -10,9 +12,31 @@ export const TabletManagement = () => {
   return (
     <>
       <BodyTitleContainer>
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+            border: "1px solid #ECECEC",
+            borderRadius: "6px",
+            boxShadow: "none",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="검색할 내용을 입력해주세요."
+            inputProps={{ "aria-label": "검색할 내용을 입력해주세요." }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <Search />
+          </IconButton>
+        </Paper>
         <div>
           <Title variant="h1">태블릿 병상 관리</Title>
         </div>
+        <div style={{ width: "400px" }}></div>
       </BodyTitleContainer>
       <TabletManagementTable />
       <PaginationContainer>
@@ -30,7 +54,7 @@ export const TabletManagement = () => {
 
 const BodyTitleContainer = styled(Box)({
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-between",
   alignItems: "center",
   margin: "60px 0 40px 0",
 });
