0b28a904b7508972610faf4e4e577ba28073e25b
Ahnseungc <omnipoo@Naver.com>
2024-10-26 15:35:52 +0900

    Design: 체크박스 추가

--- src/components/admin/admininout/adminTable.tsx
+++ src/components/admin/admininout/adminTable.tsx
@@ -2,7 +2,11 @@ import { FC } from "react";
 import styled from "@emotion/styled";
 import palette from "@styles/palette";
 import { CComboBox } from "@components/common/atom/C-ComboBox";
+import Checkbox from "@mui/material/Checkbox";
 import CInput from "@components/common/atom/C-Input";
+import { ReactComponent as CheckedIcon } from "@assets/checked-icon.svg";
+import { SvgIcon } from "@mui/material";
+
 const columns = [
   { field: "Section", headerName: "구역" },
   { field: "TableName", headerName: "태블릿 이름" },
@@ -46,6 +50,49 @@ const AdminTable: FC = () => {
               </td>
               <td>
                 <ComBoxLayout>
+                  <Checkbox
+                    icon={
+                      <div
+                        style={{
+                          width: "20px",
+                          height: "20px",
+                          border: `1px solid ${palette.divider}`,
+                          borderRadius: "4px",
+                        }}
+                      />
+                    }
+                    checkedIcon={
+                      <div
+                        style={{
+                          width: "20px",
+                          height: "20px",
+                          backgroundColor: `${palette.primary.main}`,
+                          borderRadius: "4px",
+                          position: "relative",
+                        }}
+                      >
+                        <SvgIcon
+                          component={CheckedIcon}
+                          inheritViewBox
+                          sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            width: "15px",
+                            height: "12px",
+                            position: "absolute",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                          }}
+                        />
+                      </div>
+                    }
+                    sx={{
+                      padding: 0, // 기본 패딩 제거
+                      width: "20px",
+                      height: "20px",
+                    }}
+                  />
                   <CInput
                     variant={"outlined"}
                     placeholder={"이름"}
@@ -109,4 +156,7 @@ const ComBoxLayout = styled.div`
   width: 224px;
   height: 36px;
   margin: 0 auto;
+  display: flex;
+  align-items: center;
+  gap: 13px;
 `;
