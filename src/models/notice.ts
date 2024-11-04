aa1c362c06b10b076260a2d77521531b9e3c5957
Ahnseungc <omnipoo@Naver.com>
2024-11-04 19:14:35 +0900

    .

--- src/models/notice.ts
+++ src/models/notice.ts
@@ -1,8 +1,19 @@
-export type NoticeType = {
+export type NoticeReqType = {
   wardId: number;
   recipient?: string;
   title: string;
   content: string;
   fileUrl: string;
   lastModifiedAt?: string;
+  recipientIds: Array<number>;
+};
+
+export type NoticeType = {
+  id: number;
+  staffId: number;
+  title: string;
+  content: string;
+  fileUrl: string;
+  createdAt: string;
+  lastModifiedAt: string;
 };
