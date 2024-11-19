874590aea7f699ac93dca691128e912efd43282c
dvp-tae <th3016@naver.com>
2024-11-19 14:19:12 +0900

    feat: 태블릿 리스트 api 연동

--- src/components/settings/TabletManagementTable.tsx
+++ src/components/settings/TabletManagementTable.tsx
@@ -7,6 +7,7 @@ import CInput from "@components/common/atom/C-Input";
 import { ReactComponent as Leave } from "@/assets/Leave.svg";
 import { ReactComponent as Sleep } from "@/assets/sleep.svg";
 import { Checkbox, Typography } from "@mui/material";
+import { useGetWardTabletList } from "@hooks/queries/useGetWardTabletList";
 
 const columns = [
   { field: "check", headerName: "" },
@@ -14,18 +15,7 @@ const columns = [
   { field: "TabletName", headerName: "태블릿 이름" },
   { field: "identificationNum", headerName: "식별번호" },
   { field: "PatientName", headerName: "환자이름" },
-];
-
-const rows = [
-  { id: 1, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 2, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 3, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 4, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 5, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 6, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 7, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 8, Section: "Snow", TableName: "Jon", PatientName: 35 },
-  { id: 9, Section: "Snow", TableName: "Jon", PatientName: 35 },
+  { field: "AdmissionDate", headerName: "입원 일자" },
 ];
 
 const TabletManagementTable: FC = () => {
@@ -33,138 +23,156 @@ const TabletManagementTable: FC = () => {
   const [options, setOptions] = useState<string[]>(["구역1", "구역2", "구역3", "구역4"]);
   const [selectIndex, setSelectIndex] = useState<number[]>([]);
 
+  const { data: wardTabletList } = useGetWardTabletList();
+  const totalItems = wardTabletList?.data?.length ?? 0;
+  const selectedItems = selectIndex.length;
+
   const handleSelectAll = () => {
-    if (selectIndex.length === rows.length) {
-      setSelectIndex([]);
-    } else {
-      setSelectIndex(rows.map((_, index) => index));
+    if (wardTabletList) {
+      if (selectIndex.length === wardTabletList.data.length) {
+        setSelectIndex([]);
+      } else {
+        setSelectIndex(wardTabletList.data.map((_, index) => index));
+      }
     }
   };
 
   return (
-    <StTable>
-      <thead>
-        <tr>
-          {columns.map((column, index) => {
-            if (column.field === "check") {
-              return (
-                <th key={index}>
-                  <Checkbox
-                    {...label}
-                    sx={{
-                      "&.MuiCheckbox-root": {
-                        color: "#ECECEC",
-                      },
-                      "& .MuiSvgIcon-root": {
-                        fontSize: 28,
-                      },
-                      "&.Mui-checked": {
-                        "& .MuiSvgIcon-root": {
-                          fill: "#B4C0FF",
-                        },
-                      },
-                    }}
-                    checked={selectIndex.length === rows.length}
-                    indeterminate={selectIndex.length > 0 && selectIndex.length < rows.length}
-                    onClick={handleSelectAll}
-                  />
-                </th>
-              );
-            }
-            return <th key={index}>{column.headerName}</th>;
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((_, index) => {
-          return (
-            <tr
-              key={index}
-              style={{ backgroundColor: `${selectIndex.includes(index) ? "#EFF0F8" : "white"}` }}
-            >
-              <td>
-                <ComBoxLayout>
-                  <Checkbox
-                    {...label}
-                    sx={{
-                      "&.MuiCheckbox-root": {
-                        color: "#ECECEC",
-                      },
-                      "& .MuiSvgIcon-root": {
-                        fontSize: 28,
-                      },
-                      "&.Mui-checked": {
-                        "& .MuiSvgIcon-root": {
-                          fill: "#B4C0FF",
-                        },
-                      },
-                    }}
-                    checked={selectIndex.includes(index)}
-                    onClick={() => {
-                      setSelectIndex(prevList => {
-                        if (prevList.includes(index)) {
-                          return prevList.filter(item => item !== index);
-                        } else {
-                          return [...prevList, index];
-                        }
-                      });
-                    }}
-                  />
-                </ComBoxLayout>
-              </td>
-
-              <td>
-                <ComBoxLayout>
-                  <CComboBox
-                    placeholder={"구역"}
-                    options={options}
-                    value={""}
-                    onChange={() => null}
-                    allowCustomInput={true}
-                    onCustomInputAdd={newValue => {
-                      setOptions([...options, newValue]);
-                    }}
-                  />
-                </ComBoxLayout>
-              </td>
-              <td>
-                <ComBoxLayout>
-                  <CInput
-                    variant={"outlined"}
-                    placeholder={"태블릿이름"}
-                    onChange={() => null}
-                    value={""}
-                    disabled={false}
-                    id={""}
-                  ></CInput>
-                  <TabletButtonLayout>
-                    <Leave />
-                    <Sleep />
-                  </TabletButtonLayout>
-                </ComBoxLayout>
-              </td>
-              <td>
-                <ComBoxLayout>
-                  <Typography>식별번호</Typography>
-                </ComBoxLayout>
-              </td>
-              <td>
-                <ComBoxLayout>
-                  <CInput
-                    variant={"outlined"}
-                    placeholder={"환자 이름"}
-                    onChange={() => null}
-                    value={""}
-                    disabled={false}
-                    id={""}
-                  ></CInput>
-                </ComBoxLayout>
-              </td>
+    <>
+      {wardTabletList?.data.length === 0 ? (
+        <></>
+      ) : (
+        <StTable>
+          <thead>
+            <tr>
+              {columns.map((column, index) => {
+                if (column.field === "check") {
+                  return (
+                    <th key={index}>
+                      <Checkbox
+                        {...label}
+                        sx={{
+                          "&.MuiCheckbox-root": {
+                            color: "#ECECEC",
+                          },
+                          "& .MuiSvgIcon-root": {
+                            fontSize: 28,
+                          },
+                          "&.Mui-checked": {
+                            "& .MuiSvgIcon-root": {
+                              fill: "#B4C0FF",
+                            },
+                          },
+                        }}
+                        checked={selectIndex.length === wardTabletList?.data.length}
+                        indeterminate={selectedItems > 0 && selectedItems < totalItems}
+                        onClick={handleSelectAll}
+                      />
+                    </th>
+                  );
+                }
+                return <th key={index}>{column.headerName}</th>;
+              })}
             </tr>
-          );
-        })}
-      </tbody>
-    </StTable>
+          </thead>
+          <tbody>
+            {wardTabletList?.data.map((row, index) => {
+              return (
+                <tr
+                  key={index}
+                  style={{
+                    backgroundColor: `${selectIndex.includes(index) ? "#EFF0F8" : "white"}`,
+                  }}
+                >
+                  <td>
+                    <ComBoxLayout>
+                      <Checkbox
+                        {...label}
+                        sx={{
+                          "&.MuiCheckbox-root": {
+                            color: "#ECECEC",
+                          },
+                          "& .MuiSvgIcon-root": {
+                            fontSize: 28,
+                          },
+                          "&.Mui-checked": {
+                            "& .MuiSvgIcon-root": {
+                              fill: "#B4C0FF",
+                            },
+                          },
+                        }}
+                        checked={selectIndex.includes(index)}
+                        onClick={() => {
+                          setSelectIndex(prevList => {
+                            if (prevList.includes(index)) {
+                              return prevList.filter(item => item !== index);
+                            } else {
+                              return [...prevList, index];
+                            }
+                          });
+                        }}
+                      />
+                    </ComBoxLayout>
+                  </td>
+                  <td>
+                    <ComBoxLayout>
+                      <CComboBox
+                        placeholder={"구역"}
+                        options={options}
+                        value={""}
+                        onChange={() => null}
+                        allowCustomInput={true}
+                        onCustomInputAdd={newValue => {
+                          setOptions([...options, newValue]);
+                        }}
+                      />
+                    </ComBoxLayout>
+                  </td>
+                  <td>
+                    <ComBoxLayout>
+                      <CInput
+                        variant={"outlined"}
+                        placeholder={"태블릿이름"}
+                        onChange={() => null}
+                        value={row.tabletName}
+                        disabled={false}
+                        id={""}
+                      ></CInput>
+                      <TabletButtonLayout>
+                        <Leave />
+                        <Sleep />
+                      </TabletButtonLayout>
+                    </ComBoxLayout>
+                  </td>
+                  <td>
+                    <ComBoxLayout>
+                      <Typography>{row.serialNumber}</Typography>
+                    </ComBoxLayout>
+                  </td>
+                  <td>
+                    <ComBoxLayout>
+                      <CInput
+                        variant={"outlined"}
+                        placeholder={"환자 이름"}
+                        onChange={() => null}
+                        value={row.patientName}
+                        disabled={false}
+                        id={""}
+                      ></CInput>
+                    </ComBoxLayout>
+                  </td>
+                  <td>
+                    <ComBoxLayout>
+                      <Typography>입원 일자</Typography>
+                    </ComBoxLayout>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </StTable>
+      )}
+    </>
   );
 };
 
@@ -204,7 +212,7 @@ const ComBoxLayout = styled.div`
 const TabletButtonLayout = styled.div`
   position: absolute;
   top: 4px;
-  right: -70px;
+  right: -90px;
 
   display: flex;
   gap: 5px;
---
+++ src/hooks/queries/useGetWardTabletList.ts
@@ -0,0 +1,30 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import axiosInstance from "@utils/axios/axiosInstance";
+import { AxiosError } from "axios";
+
+export type WardTabledListType = {
+  areaId: number;
+  areaName: string;
+  tabletId: number;
+  tabletName: string;
+  serialNumber: string;
+  patientId: number;
+  patientName: string;
+};
+
+export type GetWardTabletListResponse = {
+  data: WardTabledListType[];
+};
+
+export const getWardTabledList = async () => {
+  const res = await axiosInstance.get("/wards/tablet-list");
+  console.log(res.data);
+  return res.data;
+};
+
+export const useGetWardTabletList = (): UseQueryResult<GetWardTabletListResponse, AxiosError> => {
+  return useQuery<GetWardTabletListResponse, AxiosError>({
+    queryKey: ["ward-tablet-list"],
+    queryFn: getWardTabledList,
+  });
+};
