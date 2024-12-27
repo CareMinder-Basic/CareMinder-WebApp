a47b80e992572a9d19d43650f5c10ca2e577a4bf
dvp-tae <th3016@naver.com>
2024-12-27 21:39:25 +0900

    feat: 수면 모드 기능 구현

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,6 +1,6 @@
-// export const SEVER_URL = "http://223.130.146.69:8080/api";
-// export const SW_SEVER_URL = "http://223.130.146.69:8080/ws";
+export const SEVER_URL = "http://223.130.146.69:8080/api";
+export const SW_SEVER_URL = "http://223.130.146.69:8080/ws";
 
 /** 배포 서버 주소 */
-export const SEVER_URL = "https://careminder.shop/api";
-export const SW_SEVER_URL = "https://careminder.shop/ws";
+// export const SEVER_URL = "https://careminder.shop/api";
+// export const SW_SEVER_URL = "https://careminder.shop/ws";
