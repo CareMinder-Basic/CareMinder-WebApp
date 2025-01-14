9e9b8a94b60bf320e4e936174e50d3e9409e48fa
JM_DEV <48830509+wjdalsdk70@users.noreply.github.com>
2025-01-14 15:32:20 +0900

    Update baseUrl.ts

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -7,5 +7,5 @@
 // export const SW_SEVER_URL = "https://dev.careflow.co.kr/ws";
 
 /** 메인 서버 주소 */
-export const SEVER_URL = "https://careminder.shop/api";
-export const SW_SEVER_URL = "https://careminder.shop/ws";
+export const SEVER_URL = "https://careflow.co.kr/api";
+export const SW_SEVER_URL = "https://careflow.co.kr/ws";
